9b18e49df0b2a6329c0852b83c9fc4c418c27766
Rubi Torres Armendar <Rubi Torres Armendar@LAPTOP-7I4J4QD1>
2021-10-11 21:29:27 -0500

    Agregar archivos de proyecto.

---
+++ AppConsole3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppConsole3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto3D a;
+            a = new Punto3D(524,.255,564.25);
+            Console.WriteLine(a.ToString());
+            Console.ReadKey();
+        }
+    }
+}
+//Math.Sqrt(( x2-x)^2+(y2-y)^2) es la formula para calcular la distancia pero como
+//no termine la clase no la agregue por que me aparecian mucjhos errores de compilación
---
+++ AppConsole3/Punto3D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConsole3
+{
+    class Punto3D : Punto2D
+    {
+        protected double z;
+
+        public double Z
+        {
+            get 
+            {
+                return z; 
+            }
+            set 
+            { 
+                z = value; 
+            }
+        }
+         
+        public Punto3D() : base()
+        {
+            z = 0;
+        }
+        public Punto3D(double x, double y, double z) : base(x, y)
+        {
+            this.z = z;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " Z=" + z;
+        }
+    }
+}
