a53b7a37f9ce7a9e93d1ef9aca77c5aa7904b0aa
RubiTorresArmendariz <89751417+Fulanitos1208@users.noreply.github.com>
2021-10-16 19:08:42 -0500

    Update Punto3D.cs
    Se agrego abs

--- AppConsole3/Punto3D.cs
+++ AppConsole3/Punto3D.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace AppConsole3
 {
     class Punto3D : Punto2D
-    {
+        {
         protected double z;
 
         public double Z
@@ -28,9 +28,22 @@ namespace AppConsole3
         {
             this.z = z;
         }
+        public double Distancia(Punto3D pp)
+        {
+            double di = 0;
+            double dx = x - pp.x;
+            double dy = y - pp.y;
+            double dz = z - pp.z;
+            double cdx = Math.Pow(dx, 2);
+            double cdy = Math.Pow(dy, 2);
+            double cdz = Math.Pow(dz, 2);
+            di = Math.Sqrt(cdx + cdy + cdz);
+            return di;
+        }
         public override string ToString()
         {
             return base.ToString() + " Z=" + z;
         }
     }
 }
+
